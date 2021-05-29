593b85703c47cfc1c77250593c7b5e95ef3e74b7
Nicolae <67197181+Nicolae77@users.noreply.github.com>
2021-05-29 11:19:42 +0100

    Update main.py

--- CSV Python/main.py
+++ CSV Python/main.py
@@ -32,13 +32,5 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
         
-         if i in all_states:
-        guessed_states.append(answer_state)
-        t = turtle.Turtle()
-        t.hideturtle()
-        t.penup()
-        state_data = data[data.state == answer_state]
-        t.goto(int(state_data.x), int(state_data.y))
-        t.write(answer_state)
      
 
